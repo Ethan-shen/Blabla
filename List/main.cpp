116ede00c192e9362e94c07d0e7468776568f7e1
Ethan shen <1406483367@qq.com>
2016-04-17 22:40:03 +0800

    Added files via upload

---
+++ List/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "list.cpp"
+#include <list>
+using namespace std;
+int main()
+{
+    List<int> a;
+    for(int i = 1; i <= 5;++i)
+        a.push_back(i);
+    cout << a._size() << endl;
+    cout << a[-1] << endl;
+
+
+
+
+    return 0;
+
+}
